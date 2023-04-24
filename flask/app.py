b732ca5fd160d2042ce29a2be8901a97eb1a9a70
Josh Kelleran <josh.kelleran@gmail.com>
2023-04-23 22:07:41 -0700

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins="http://localhost:3000")
 
 import subprocess
-
 
 @app.route('/get-model', methods=['POST'])
 def get_model():
@@ -14,4 +15,7 @@ def get_model():
     param1 = data['param1']
     param2 = data['param2']
     subprocess.call(['python', 'my_script.py', param1, param2])
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
