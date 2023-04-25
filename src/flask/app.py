80979b5f1a47306774946aaf0f66c470a9ce7122
GrahamCPurvis <92408767+GrahamCPurvis@users.noreply.github.com>
2023-04-24 18:51:26 -0700

    Flask API Functioning
    Nice

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,20 +1,54 @@
-from flask import Flask, request
+import json
+import sys
+from pathlib import Path
+
+import get_colors as gc
 from flask_cors import CORS
+
+from flask import Flask, request
+
+gen_dir = str(Path(__file__).resolve().parents[2])
+if gen_dir not in sys.path:
+    sys.path.append(gen_dir)
+
+
 
 app = Flask(__name__)
 CORS(app, origins="http://localhost:3000")
 
+import pickle
 import subprocess
+import sys
+from pathlib import Path
+
+gen_dir = str(Path(__file__).resolve().parents[2])
+if gen_dir not in sys.path:
+    sys.path.append(gen_dir)
+
+
+models_directory = gen_dir + "/data/created_data/models/"
+models_filename = models_directory + "models_all_1_dict.pkl"
+
+with open(models_filename, "rb") as f:
+    loaded_models_dict = pickle.load(f)
+
+
+
+filename = gen_dir + '/data/created_data/input_data/output_file.json'
+with open(filename, "rb") as f:
+    geojson = json.load(f)
 
 @app.route('/get-model', methods=['POST'])
 def get_model():
     data = request.get_json()
-    print("data")
-    print(data)
-    # pass data as parameters to Python script
-    param1 = data['param1']
-    param2 = data['param2']
-    subprocess.call(['python', 'my_script.py', param1, param2])
+
+    return gc.get_colors(geojson, loaded_models_dict, data['rain'], data['temperature'], data['humidity'])
+    
+    
+    
+    
+    
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/flask/get_colors.py
@@ -0,0 +1,57 @@
+import datetime
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+
+
+def get_colors(geojson, model, prcp, temp, rhum):
+# Get the geojson from flask 
+
+    for feature in geojson['features']:
+        seg_id = feature['properties']['segment_id']
+        X_test = [[temp, 0, rhum, prcp, 0, 0, 0, 0, datetime.datetime.now().hour]]
+        
+        # Scale the feature vector using the same scaler used during training
+        scaler = StandardScaler()
+        X_test = scaler.fit_transform(X_test)
+        
+        # Predict the speed using the linear regression model and the feature vector
+        y_pred = model[int(seg_id)]['model'].predict(X_test)[0]
+        # print('predicted speed',y_pred)
+        
+        feature['properties']['speed'] = y_pred
+        feature['properties']['color'] = get_color(y_pred, 50)
+
+        
+    return geojson
+        
+# For feature in features, get sgement_id from properties
+#   Get predicted speed based off of, passed in data values from flask
+#   Add speed, and RGB color to properties 
+
+#Return this as response to front-end, which displays data and refreshes 
+
+
+
+def get_color(current_speed, historical_speed):
+    """Return an RGB array between dark red (-50% slower) and bright green (+50% faster)
+    depending on the comparison between the current speed and historical speed."""
+    
+    # Calculate the percentage difference between the current speed and historical speed
+    percentage_diff = (current_speed - historical_speed) / historical_speed * 100
+    
+    # Determine the color based on the percentage difference
+    if percentage_diff <= -50:
+        # Dark red for -50% or slower
+        return [139, 0, 0]
+    elif percentage_diff >= 50:
+        # Bright green for +50% or faster
+        return [0, 255, 0]
+    else:
+        # Calculate the color between red and green based on the percentage difference
+        red = max(0, min(255, int(255 * (100 + percentage_diff) / 100)))
+        green = max(0, min(255, int(255 * (100 - percentage_diff) / 100)))
+        blue = 0
+        return [red, green, blue]
+
